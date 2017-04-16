78b71b607f12ba503f45760ea2603bcc6e20df8d
Nikhil Kansal <nkansal96@gmail.com>
2017-04-15 20:45:36 -0700

    added leaderboard route

--- app/api/v1/leaderboard/index.js
+++ app/api/v1/leaderboard/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const error = require('../../../error');
 let router = express.Router();
+
+router.route('/')
+.get((req, res, next) => {
+    next(new error.NotImplemented());
+});
 
 module.exports = { router };
