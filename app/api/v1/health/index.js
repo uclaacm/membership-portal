e77b652cb6347ace2396f404143214a31bab3d7a
Nikhil Kansal <nkansal96@gmail.com>
2017-04-16 16:56:49 -0700

    added health route

---
+++ app/api/v1/health/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+let router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.json({
+        cpu: process.cpuUsage(),
+        memory: process.memoryUsage(),
+        uptime: process.uptime()
+    });
+});
+
+module.exports = { router };
--- app/api/v1/index.js
+++ app/api/v1/index.js
@@ -4,6 +4,7 @@ let router = express.Router();
 // Route each API
 router.use('/user', require('./user').router);
 router.use('/event', require('./event').router);
+router.use('/health', require('./health').router);
 router.use('/leaderboard', require('./leaderboard').router);
 
 module.exports = { router };
