80a315ad8c720c6513abe37db17c295f6d12f4c6
Nikhil Kansal <nkansal96@gmail.com>
2017-04-15 20:45:08 -0700

    added basic user route

--- app/api/v1/user/index.js
+++ app/api/v1/user/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const error = require('../../../error');
 let router = express.Router();
+
+router.route('/')
+.get((req, res, next) => {
+    res.json({ error: null, user: req.user.getUserProfile() });
+})
+.post((req, res, next) => {
+    next(new error.NotImplemented());
+});
 
 module.exports = { router };
