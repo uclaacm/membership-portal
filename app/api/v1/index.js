75f282d9f305eaacd071a2e570c4f936edafa55e
Nikhil Kansal <nkansal96@gmail.com>
2017-04-20 01:41:31 -0700

    added attendance route

--- app/api/v1/index.js
+++ app/api/v1/index.js
@@ -5,6 +5,7 @@ let router = express.Router();
 router.use('/user', require('./user').router);
 router.use('/event', require('./event').router);
 router.use('/health', require('./health').router);
+router.use('/attendance', require('./attendance').router);
 router.use('/leaderboard', require('./leaderboard').router);
 
 module.exports = { router };
