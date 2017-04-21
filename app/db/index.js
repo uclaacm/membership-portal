f089e33d32f0c88c96a973de022775c6edf5a9ec
Nikhil Kansal <nkansal96@gmail.com>
2017-04-21 16:27:22 -0700

    added admin account for development

--- app/db/index.js
+++ app/db/index.js
@@ -7,14 +7,27 @@ let db = new Sequelize(config.database.db, config.database.user, config.database
     dialect: 'postgres',
     host: config.database.host,
     logging: config.isDevelopment ? logger.debug : false
-})
+});
 
 let User = require('./schema/user')(Sequelize, db);
 let Event = require('./schema/event')(Sequelize, db);
 let Attendance = require('./schema/attendance')(Sequelize, db);
 
-let setup = (force) => {
-    db.sync({ force: !!force });
+let setup = () => {
+    db.sync().then(() => {
+        if (config.isDevelopment) {
+            User.findOrCreate({ where: { email: 'admin' }, defaults: {
+                email: 'admin',
+                accessType: 'ADMIN',
+                state: 'ACTIVE',
+                firstName: 'Nikhil',
+                lastName: 'Kansal',
+                hash: '$2a$10$db7eYhWGZ1LZl27gvyX/iOgb33ji1PHY5.pPzRyXaNlbctCFWMF9G', //test1234
+                year: 2,
+                major: 'Computer Science'
+            }});
+        }
+    });
 };
 
 let errorHandler = (err, req, res, next) => {
