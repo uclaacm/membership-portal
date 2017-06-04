737df0fe7c9b04136f2d4394f5bbebd04713beaa
Ram Goli <ramsgoli@gmail.com>
2017-06-04 14:38:01 -0700

    change data.scoreboard to data.leaderboard

--- app/api/v1/leaderboard/index.js
+++ app/api/v1/leaderboard/index.js
@@ -7,9 +7,9 @@ router.route('/')
 .get((req, res, next) => {
 	let offset = parseInt(req.query.offset);
 	let limit = parseInt(req.query.limit);
-	
+
 	User.getLeaderboard(offset, limit).then(users => {
-		res.json({ error: null, scoreboard: users.map(u => u.getUserProfile()) });
+		res.json({ error: null, leaderboard: users.map(u => u.getUserProfile()) });
 	}).catch(next);
 });
 
