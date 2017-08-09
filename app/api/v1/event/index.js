78f0bbfa4cea9dc6e21a48a831e9cd8b7f2d6d77
Nikhil Kansal <nkansal96@gmail.com>
2017-08-09 12:23:46 -0700

    allow admin to view attendance code

--- app/api/v1/event/index.js
+++ app/api/v1/event/index.js
@@ -28,11 +28,11 @@ router.route('/:uuid?')
 		let offset = parseInt(req.query.offset);
 		let limit = parseInt(req.query.limit);
 		Event.getAll(offset, limit).then(events => {
-			res.json({ error: null, events: events.map(e => e.getPublic()) });
+			res.json({ error: null, events: events.map(e => e.getPublic(req.user.isAdmin())) });
 		}).catch(next);
 	} else {
 		Event.findByUUID(req.params.uuid).then(event => {
-			res.json({ error: null, event: event ? event.getPublic() : null });
+			res.json({ error: null, event: event ? event.getPublic(req.user.isAdmin()) : null });
 		}).catch(next);
 	}
 })
@@ -44,7 +44,10 @@ router.route('/:uuid?')
 .post((req, res, next) => {
 	if (req.params.uuid || !req.body.event)
 		return next(new error.BadRequest());
-	
+
+	if (req.body.event.startDate && req.body.event.endDate && new Date(req.body.event.startDate) > new Date(req.body.event.endDate))
+		return next(new error.BadRequest("Start date must be before end date"));
+
 	Event.create(Event.sanitize(req.body.event)).then(event => {
 		res.json({ error: null, event: event.getPublic() });
 	}).catch(next);
@@ -55,7 +58,6 @@ router.route('/:uuid?')
 
 	if (req.body.event.startDate && req.body.event.endDate && new Date(req.body.event.startDate) > new Date(req.body.event.endDate))
 		return next(new error.BadRequest("Start date must be before end date"));
-
 
 	Event.findByUUID(req.params.uuid).then(event => {
 		if (!event)
