f488bb2e5b2cd8a622226b47af83df6a7e0427ce
Nikhil Kansal <nkansal96@gmail.com>
2017-08-14 14:44:13 -0700

    update static, activity response

--- app/api/v1/activity/index.js
+++ app/api/v1/activity/index.js
@@ -4,8 +4,8 @@ const { Activity } = require('../../../db');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  Activity.getPublicStream(req.user.uuid).then(activities => {
-    res.json({ error: null, activities: activities.map(a => a.getPublic()) });
+  Activity.getPublicStream(req.user.uuid).then(activity => {
+    res.json({ error: null, activity: activity.map(a => a.getPublic()) });
   }).catch(next);
 });
 
