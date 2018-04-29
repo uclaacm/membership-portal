96c4e37d1b704104944ee9429641000d35c505f9
ktan17 <Kevin.Tan@gpiglobal.com>
2018-04-29 15:09:27 -0700

    completed initial implementation of basic analytics

--- app/api/v1/analytics/events.js
+++ app/api/v1/analytics/events.js
@@ -1,32 +1,39 @@
 const _ = require('underscore')
 const express = require('express')
-const { Event, Attendance } = require('../../../db');
+const { Event, Attendance, User } = require('../../../db');
 let router = express.Router();
 
 // route each API version
 router.route('/')
 .get((req, res, next) => {
 	let analytics = [];
+	let userCounts = [];
 	Event.getAll()
 	.then(events => {
-		let attendances = events.map(event => {
-			analytics.push({
-				event: event.getPublic()
-			});
-			return Attendance.getAttendanceForEvent(event.uuid);
-		});
-		return Promise.all(attendances);
+		return Promise.all(events.map(getAnalyticsForEvent));
 	})
-	.then(results => {
-		results.forEach((result, index) => {
-			console.log(index, result);
-			analytics[index].analytics = {
-				attendance: result.length
-			};
-		});
-		res.json(analytics);
+	.then(analytics => {
+		res.json({ error: null, analytics });
 	})
 	.catch(next);
 });
+
+function getAnalyticsForEvent(event) {
+	return Attendance.getAttendanceForEvent(event.uuid)
+	.then(attendance => {
+		users = attendance.map(singleAttendance => User.findByUUID(singleAttendance.user))
+		return Promise.all(users)
+	})
+	.then(users => {
+		return {
+			event,
+			analytics: {
+				majors: _.countBy(users, user => user.major),
+				years: _.countBy(users, user => user.year),
+				attendance: users.map(user => user.getUserProfile())
+			}
+		};
+	})
+}
 
 module.exports = { router };
