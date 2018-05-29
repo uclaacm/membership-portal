c918a50da8721e90109adbadd69471e42c7c2798
Helen Lee <helenhyewonlee@gmail.com>
2018-05-28 18:06:43 -0700

    implemented committee leaderboard

--- app/api/v1/leaderboard/index.js
+++ app/api/v1/leaderboard/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const error = require('../../../error');
-const { User } = require('../../../db');
+const { User, Event, Attendance } = require('../../../db');
 const router = express.Router();
+const _ = require('underscore');
 
 /**
  * Get the leaderboard.
@@ -15,9 +16,57 @@ router.route('/')
 	const limit = parseInt(req.query.limit);
 	
 	User.getLeaderboard(offset, limit).then(users => {
-		// map the user objects to public (just name, picture, and points) for privacy reasons
 		res.json({ error: null, leaderboard: users.map(u => u.getUserProfile()) });
 	}).catch(next);
 });
+
+/*
+ * TODO: support pagination using 'offset' and 'limit' query
+ */
+router.route('/committee')
+.get((req, res, next) => {
+	const start = new Date(0);
+	const end = new Date();
+	const committee = 'Hack';
+	const allEvents = [];
+	const attendeePoints = [];
+
+	Event.getByFilters({ start, end, committee }).then(events => {
+		const attendees = [];
+		events.forEach(event => {
+			allEvents.push({ event: event.getPublic() });
+			attendees.push(Attendance.getAttendanceForEvent(event.uuid));
+		});
+		return Promise.all(attendees);
+	})
+	.then(allAttendees => {
+		let attendeeProfiles = [];
+		let names = [];
+		allAttendees.forEach((result, index) => {
+			names = result.map(res => res.user);
+			names.forEach((userName) => {
+				attendeePoints.push({ user: userName, points: parseInt(allEvents[index].event.attendancePoints) });		
+			});
+		});
+
+		names = _.uniq(names);
+		names.forEach((userName) => {
+			attendeeProfiles.push(User.findByUUID(userName));
+		});
+		return Promise.all(attendeeProfiles);
+	}) 
+	.then(attendeeProfiles => {
+		attendeeProfiles.map(user => user.getUserProfile());
+		attendeeProfiles.forEach((user, index) => {
+			user.points = 0;
+		});
+
+		attendeePoints.forEach((userName) => {
+			attendeeProfiles.find(u => u.uuid == userName.user).points += userName.points;
+		});
+		res.json(_.sortBy(attendeeProfiles, 'points').reverse());
+	})
+	.catch(next);
+})
 
 module.exports = { router };
