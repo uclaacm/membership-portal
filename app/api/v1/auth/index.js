db235c6f8d905f8f8dcb9b1d7d23bdbc284211d8
Dylon Tjanaka <dylon.tjanaka@gmail.com>
2021-11-14 04:17:45 -0800

    Partially fix login.

--- app/api/v1/auth/index.js
+++ app/api/v1/auth/index.js
@@ -5,8 +5,10 @@ const error = require('../../../error');
 const log = require('../../../logger');
 const Mail = require('../../../mail');
 const { User, Activity } = require('../../../db');
+
 const router = express.Router();
 const { OAuth2Client } = require('google-auth-library');
+
 const client = new OAuth2Client(config.google.clientId);
 
 const TOKEN_EXPIRES = 86400; // 1 day in seconds
@@ -14,99 +16,127 @@ const TOKEN_EXPIRES = 86400; // 1 day in seconds
 /**
  * Middleware function that determines whether or not a user is authenticated
  * and assigns the req.user object to their user info from the db
- * 
+ *
  * @param req The request object
  * @param res The response object
  * @param next The next-middleware function
  */
 const authenticated = (req, res, next) => {
-	// We're looking for a header in the form of:
-	//   Authorization: Bearer <TOKEN>
-	const authHeader = req.get('Authorization');
-	if (!authHeader)
-		return next(new error.Unauthorized());
+  // We're looking for a header in the form of:
+  //   Authorization: Bearer <TOKEN>
+  const authHeader = req.get('Authorization');
+  if (!authHeader) return next(new error.Unauthorized());
 
-	// authHead should be in the form of ['Bearer', '<TOKEN>']
-	const authHead = authHeader.split(' ');
-	if(authHead.length != 2 || authHead[0] !== 'Bearer' || authHead[1].length < 1)
-		return next(new error.Unauthorized());
+  // authHead should be in the form of ['Bearer', '<TOKEN>']
+  const authHead = authHeader.split(' ');
+  if (
+    authHead.length !== 2
+    || authHead[0] !== 'Bearer'
+    || authHead[1].length < 1
+  ) return next(new error.Unauthorized());
 
-	const token = authHead[1];
-	jwt.verify(token, config.session.secret, (err, decoded) => {
-		if(err)
-			return next(new error.Unauthorized());
+  const token = authHead[1];
+  jwt.verify(token, config.session.secret, (err, decoded) => {
+    if (err) return next(new error.Unauthorized());
 
-		// if the user provided a valid token, use it to deserialize the UUID to
-		// an actual user object
-		User.findByUUID(decoded.uuid).then(user => {
-			if (!user)
-				throw new error.Unauthorized();
-			req.user = user;
-		}).then(next).catch(next);
-	});
+    // if the user provided a valid token, use it to deserialize the UUID to
+    // an actual user object
+    User.findByUUID(decoded.uuid)
+      .then((user) => {
+        if (!user) throw new error.Unauthorized();
+        req.user = user;
+      })
+      .then(next)
+      .catch(next);
+  });
 };
 
 /**
  * Login route.
- * 
+ *
  * POST body should be in the format of { email, password }
  * On success, this route will return a token
  */
-router.post("/login", (req, res, next) => {
-	if(!req.body.tokenId || req.body.tokenId.length < 1)
-		return next(new error.BadRequest('Invalid token.'));
+router.post('/login', (req, res, next) => {
+  if (!req.body.tokenId || req.body.tokenId.length < 1) return next(new error.BadRequest('Invalid token.'));
 
-	client.verifyIdToken({
-        idToken: req.body.tokenId,
-        audience: config.google.clientId
-    }).then(ticket => {
-		const { given_name, family_name, email, picture, googleId } = ticket.getPayload(); 
+  const createUserToken = (user) => {
+	// create a token with the user's ID and privilege level
+	jwt.sign(
+	  {
+		uuid: user.getDataValue('uuid'),
+		admin: user.isAdmin(),
+	  },
+	  config.session.secret,
+	  { expiresIn: TOKEN_EXPIRES },
+	  (err, token) => {
+		if (err) return next(err);
+  
+		// respond with the token upon successful login
+		res.json({
+		  error: null,
+		  user: user.getPublicProfile(),
+		  token,
+		});
+		// register that the user logged in
+		Activity.accountLoggedIn(user.uuid);
+	  },
+	);
+  };
 
-		User.findByEmail(email.toLowerCase()).then((user)=>{
+  client
+    .verifyIdToken({
+      idToken: req.body.tokenId,
+      audience: config.google.clientId,
+    })
+    .then((ticket) => {
+      const { email } = ticket.getPayload();
 
-			if (!user) {
-				
-				// TODO: implement email auth instead of just active
-				// get a sanitized version of the input
-				let userModel = {
-					'profileId': googleId,
-					'email': email.toLowerCase(),
-					'firstName': given_name,
-					'lastName': family_name,
-					'picture': picture,
-					'state': 'ACTIVE',
-				}
-				
-				User.create(userModel).then(user => {
-					// register the account creation as the user's first activity
-					Activity.accountCreated(user.uuid);
-					// responsd with the newly created user
-					return user;
-				}).catch(next);
-			}
+      User.findByEmail(email.toLowerCase())
+        .then(user => ({ user, ticket }))
+        .then((userData) => {
+          const { user, ticket } = userData;
+          const {
+            given_name, family_name, email, picture, googleId,
+          } = ticket.getPayload();
 
-			if (user.isBlocked())
-				throw new error.Forbidden('Your account has been blocked');
-			
-			return user;
+          if (!user) {
+            // TODO: implement email auth instead of just active
+            // get a sanitized version of the input
+            const userModel = {
+              profileId: googleId,
+              email: email.toLowerCase(),
+              firstName: given_name,
+              lastName: family_name,
+              picture,
+              state: 'ACTIVE',
+              // PLACEHOLDERS - need to add placeholders or make them nullable and then require they be added later...
+              // or have a flow that requires a user to fill these in right after signing in the first time
+              year: 1,
+              major: 'underwater basketweaving',
+            };
 
-		}).then(user => {
-			// create a token with the user's ID and privilege level
-			return jwt.sign({
-				uuid  : user.getDataValue('uuid'),
-				admin : user.isAdmin()
-			}, config.session.secret, { expiresIn: TOKEN_EXPIRES }, (err, token) => {
-				if (err) 
-					return next(err);
+            User.create(userModel)
+              .then((user) => {
+                if (user && user.isBlocked()) throw new error.Forbidden('Your account has been blocked');
+                // register the account creation as the user's first activity
+                Activity.accountCreated(user.uuid);
+                // responds with the newly created user
+                return user;
+              })
+              .then(user => createUserToken(user)).catch(next);
 
-				// respond with the token upon successful login
-				res.json({ error: null, user: user.getPublicProfile(), token: token });
-				// register that the user logged in
-				Activity.accountLoggedIn(userInfo.uuid);
-			});
-			
-		}).catch(next);
-	}).catch(next);
+            return null; // we don't care about result (http://goo.gl/rRqMUw)
+          }
+          if (user && user.isBlocked()) throw new error.Forbidden('Your account has been blocked');
+          else {
+            createUserToken(user);
+
+            return null; // we don't care about result (http://goo.gl/rRqMUw)
+          }
+        })
+        .catch(next);
+    })
+    .catch(next);
 });
-
 module.exports = { router, authenticated };
--- app/db/schema/user.js
+++ app/db/schema/user.js
@@ -1,217 +1,221 @@
 const _ = require('underscore');
 
 module.exports = (Sequelize, db) => {
-	const User = db.define('user', {
-		id: {
-			type: Sequelize.INTEGER,
-			autoIncrement: true,
-			primaryKey: true
-		},
+  const User = db.define('user', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
 
-		// user ID: main way of querying the user
-		uuid: {
-			type: Sequelize.UUID,
-			defaultValue: Sequelize.UUIDV4
-		},
+    // user ID: main way of querying the user
+    uuid: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
 
-		// facebook profile of the user: used to generate profile picture
-		profileId: {
-			type: Sequelize.STRING
-		},
+    // email address of the user
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'The email you entered is not valid',
+        },
+        notEmpty: {
+          msg: 'The email is a required field',
+        },
+      },
+    },
 
-		// email address of the user
-		email: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			unique: true,
-			validate: {
-				isEmail: {
-					msg: "The email you entered is not valid"
-				},
-				notEmpty: {
-					msg: "The email is a required field"
-				}
-			}
-		},
+    // type of account
+    //   RESTRICTED - not used currently
+    //   STANDARD   - a regular member
+    //   ADMIN      - admin type user
+    accessType: {
+      type: Sequelize.ENUM('RESTRICTED', 'STANDARD', 'ADMIN'),
+      defaultValue: 'STANDARD',
+    },
 
-		// type of account
-		//   RESTRICTED - not used currently
-		//   STANDARD   - a regular member
-		//   ADMIN      - admin type user
-		accessType: {
-			type: Sequelize.ENUM('RESTRICTED','STANDARD','ADMIN'),
-			defaultValue: 'STANDARD'
-		},
+    // user's first name
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2, 255],
+          msg: 'Your first name must be at least 2 characters long',
+        },
+        notEmpty: {
+          msg: 'The first name is a required field',
+        },
+      },
+    },
 
-		// user's first name
-		firstName: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			validate: {
-				len: {
-					args: [2, 255],
-					msg: "Your first name must be at least 2 characters long"
-				},
-				notEmpty: {
-					msg: "The first name is a required field"
-				}
-			}
-		},
+    // user's last name
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2, 255],
+          msg: 'Your last name must be at least 2 characters long',
+        },
+        notEmpty: {
+          msg: 'The last name is a required field',
+        },
+      },
+    },
 
-		// user's last name
-		lastName: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			validate: {
-				len: {
-					args: [2, 255],
-					msg: "Your last name must be at least 2 characters long"
-				},
-				notEmpty: {
-					msg: "The last name is a required field"
-				}
-			}
-		},
+    picture: {
+      type: Sequelize.STRING,
+    },
 
-		picture: {
-			type: Sequelize.STRING,
-		},
+    // user's year
+    //   typical mapping: [1,2,3,4,5] => [Freshman,Sophomore,Junior,Senior,Post-senior]
+    year: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [[1, 2, 3, 4, 5]],
+          msg: 'Your year must be one of [1, 2, 3, 4, 5]',
+        },
+        notEmpty: {
+          msg: 'The year is a required field',
+        },
+      },
+    },
 
-		// user's year
-		//   typical mapping: [1,2,3,4,5] => [Freshman,Sophomore,Junior,Senior,Post-senior]
-		year: {
-			type: Sequelize.INTEGER,
-			allowNull: false,
-			validate: {
-				isIn: {
-					args: [[1, 2, 3, 4, 5]],
-					msg: "Your year must be one of [1, 2, 3, 4, 5]"
-				},
-				notEmpty: {
-					msg: "The year is a required field"
-				}
-			}
-		},
+    // user's major
+    major: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2, 255],
+          msg: 'Your major must be at least 2 characters long',
+        },
+        notEmpty: {
+          msg: 'The major is a required field',
+        },
+      },
+    },
 
-		// user's major
-		major: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			validate: {
-				len: {
-					args: [2, 255],
-					msg: "Your major must be at least 2 characters long"
-				},
-				notEmpty: {
-					msg: "The major is a required field"
-				}
-			}
-		},
+    // amount of points the user has
+    points: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
 
-		// amount of points the user has
-		points: {
-			type: Sequelize.INTEGER,
-			defaultValue: 0
-		},
+    // date of last login
+    lastLogin: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  }, {
+    // creating indices on frequently accessed fields improves efficiency
+    indexes: [
+      // a hash index on the uuid makes lookup by UUID O(1)
+      {
+        unique: true,
+        fields: ['uuid'],
+      },
 
-		// date of last login
-		lastLogin: {
-			type: Sequelize.DATE,
-			defaultValue: Sequelize.NOW
-		}
-	}, {
-		// creating indices on frequently accessed fields improves efficiency
-		indexes: [
-			// a hash index on the uuid makes lookup by UUID O(1)
-			{
-				unique: true,
-				fields: ['uuid']
-			},
+      // a hash index on the email makes lookup by email O(1)
+      {
+        unique: true,
+        fields: ['email'],
+      },
 
-			// a hash index on the email makes lookup by email O(1)
-			{
-				unique: true,
-				fields: ['email']
-			},
+      // a hash index on the access code makes lookup by access code O(1)
+      {
+        unique: true,
+        fields: ['accessCode'],
+      },
 
-			// a hash index on the access code makes lookup by access code O(1)
-			{
-				unique: true,
-				fields: ['accessCode']
-			},
+      // a BTREE index on the uuid makes retrieving the leaderboard O(N)
+      {
+        name: 'user_points_btree_index',
+        method: 'BTREE',
+        fields: ['points', { attribute: 'points', order: 'DESC' }],
+      },
+    ],
+  });
 
-			// a BTREE index on the uuid makes retrieving the leaderboard O(N)
-			{
-				name: 'user_points_btree_index',
-				method: 'BTREE',
-				fields: ['points', { attribute: 'points', order: 'DESC' }]
-			}
-		]
-	});
+  User.findByUUID = function (uuid) {
+    return this.findOne({ where: { uuid } });
+  };
 
-	User.findByUUID = function(uuid) {
-		return this.findOne({ where : { uuid } });
-	};
+  User.findByEmail = function (email) {
+    return this.findOne({ where: { email } });
+  };
 
-	User.findByEmail = function(email) {
-		return this.findOne({ where : { email } });
-	};
+  User.findByAccessCode = function (accessCode) {
+    return this.findOne({ where: { accessCode } });
+  };
 
-	User.findByAccessCode = function(accessCode) {
-		return this.findOne({ where : { accessCode } });
-	};
+  User.getLeaderboard = function (offset, limit) {
+    if (!offset || offset < 0) offset = 0;
+    if (!limit || limit < 0) limit = undefined;
+    return this.findAll({
+      where: { accessType: 'STANDARD' }, offset, limit, order: [['points', 'DESC']],
+    });
+  };
 
-	User.getLeaderboard = function(offset, limit) {
-		if (!offset || offset < 0) offset = 0;
-		if (!limit || limit < 0)  limit = undefined;
-		return this.findAll({ where: { accessType: 'STANDARD' }, offset, limit, order: [['points', 'DESC']] });
-	};
+  User.sanitize = function (user) {
+    user = _.pick(user, ['profileId', 'email', 'firstName', 'lastName', 'year', 'major']);
+    if (user.email) user.email = user.email.toLowerCase();
+    return user;
+  };
 
-	User.sanitize = function(user) {
-		user = _.pick(user, ['profileId', 'email', 'firstName', 'lastName', 'year', 'major']);
-		if (user.email)
-			user.email = user.email.toLowerCase();
-		return user;
-	};
+  User.prototype.addPoints = function (points) {
+    return this.increment({ points });
+  };
 
-	User.prototype.addPoints = function(points) {
-		return this.increment({ points });
-	};
+  User.prototype.getPublicProfile = function () {
+    return {
+      firstName: this.getDataValue('firstName'),
+      lastName: this.getDataValue('lastName'),
+      picture: this.getDataValue('picture'),
+      points: this.getDataValue('points'),
+    };
+  };
 
-	User.prototype.getPublicProfile = function() {
-		return {
-			firstName : this.getDataValue('firstName'),
-			lastName  : this.getDataValue('lastName'),
-			picture   : this.getDataValue('picture'),
-			points    : this.getDataValue('points'),
-		};
-	};
+  User.prototype.getUserProfile = function () {
+    const uuid = this.getDataValue('uuid');
+    return {
+      uuid,
+      firstName: this.getDataValue('firstName'),
+      lastName: this.getDataValue('lastName'),
+      picture: this.getDataValue('picture'),
+      email: this.getDataValue('email'),
+      year: this.getDataValue('year'),
+      major: this.getDataValue('major'),
+      points: this.getDataValue('points'),
+    };
+  };
 
-	User.prototype.getUserProfile = function() {
-		const uuid = this.getDataValue('uuid');
-		return {
-			uuid,
-			firstName  : this.getDataValue('firstName'),
-			lastName   : this.getDataValue('lastName'),
-			picture    : this.getDataValue('picture'),
-			email      : this.getDataValue('email'),
-			year       : this.getDataValue('year'),
-			major      : this.getDataValue('major'),
-			points     : this.getDataValue('points')
-		};
-	};
+  User.prototype.isAdmin = function () {
+    return this.getDataValue('accessType') === 'ADMIN';
+  };
 
-	User.prototype.isAdmin = function() {
-		return this.getDataValue('accessType') === 'ADMIN';
-	};
+  User.prototype.isStandard = function () {
+    return this.getDataValue('accessType') === 'STANDARD';
+  };
 
-	User.prototype.isStandard = function() {
-		return this.getDataValue('accessType') === 'STANDARD';
-	};
+  User.prototype.isRestricted = function () {
+    return this.getDataValue('accessType') === 'RESTRICTED';
+  };
 
-	User.prototype.isRestricted = function() {
-		return this.getDataValue('accessType') === 'RESTRICTED';
-	};
+  User.prototype.isActive = function () {
+    return this.getDataValue('state') === 'ACTIVE';
+  };
 
-	return User;
+  User.prototype.isBlocked = function () {
+    return this.getDataValue('state') === 'BLOCKED';
+  };
+
+  return User;
 };
