6f458060f962fea99eae0e02a05b92c42dfa7f16
Katelyn Yu <yu.katelyna@gmail.com>
2025-11-13 03:34:01 -0800

    Created validation function to not accept blank responses

--- app/api/v1/internship/models/InternshipApplication.js
+++ app/api/v1/internship/models/InternshipApplication.js
@@ -81,24 +81,17 @@ const InternshipApplicationSchema = new Schema(
       trim: true,
     },
 
-    // TODO:
-    // change this to be an array of response objects..
-    responses: [{
-      questionKey: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      question: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      answer: {
-        type: String,
-        required: true,
-        trim: true,
-      },
+    firstChoiceResponses: [{
+      question: String,
+      answer: String,
+    }],
+    secondChoiceResponses: [{
+      question: String,
+      answer: String,
+    }],
+    thirdChoiceResponses: [{
+      question: String,
+      answer: String,
     }],
 
     // Status from each of the committee choices
@@ -155,12 +148,46 @@ InternshipApplicationSchema.pre('save', function (next) {
   // Linter wants a return, but not needed because we call next()
 });
 
-// TODO:
-// Validate no blank responses
-InternshipApplicationSchema.pre('save', function(next) {
-  // Ensure no blank responses..
+// Accept no blank responses
+InternshipApplicationSchema.pre('save', function (next) {
+  const committees = [this.firstChoice, this.secondChoice, this.thirdChoice].filter(Boolean);
+
+  for (let i = 0; i < committees.length; i++) {
+    const choseCommittee = committees[i];
+    if (!choseCommittee) continue;
+
+    switch (i) {
+      case 0:
+        if (!Array.isArray(this.firstChoiceResponses) || this.firstChoiceResponses.length === 0) {
+        return next(new Error('First choice responses required'));
+        }
+        if (!this.firstChoiceResponses.every(r => r && r.question != null && r.answer != null)) {
+          return next(new Error('First choice responses cannot have blank question/answer'));
+        }
+        break;
+      case 1:
+        if (!Array.isArray(this.secondChoiceResponses) || this.secondChoiceResponses.length === 0) {
+        return next(new Error('Second choice responses required'));
+        }
+        if (!this.secondChoiceResponses.every(r => r && r.question != null && r.answer != null)) {
+          return next(new Error('Second choice responses cannot have blank question/answer'));
+        }
+        break;
+      case 2:
+        if (!Array.isArray(this.thirdChoiceResponses) || this.thirdChoiceResponses.length === 0) {
+        return next(new Error('Third choice responses required'));
+        }
+        if (!this.thirdChoiceResponses.every(r => r && r.question != null && r.answer != null)) {
+          return next(new Error('Third choice responses cannot have blank question/answer'));
+        }
+        break;
+      }
+  }
+
+  this.lastModifiedAt = Date.now();
   next();
 });
+
 
 // Create indices
 InternshipApplicationSchema.index({ userId: 1 });
@@ -168,7 +195,7 @@ InternshipApplicationSchema.index({ firstChoice: 1 });
 InternshipApplicationSchema.index({ firstChoiceStatus: 1 });
 InternshipApplicationSchema.index({ appliedAt: -1 });
 InternshipApplicationSchema.index({ userId: 1, firstChoice: 1 });
-InternshipApplicationSchema.index({ userId: 1, appliedAt: -1});
+InternshipApplicationSchema.index({ userId: 1, appliedAt: -1 });
 InternshipApplicationSchema.index({ firstChoice: 1, firstChoiceStatus: 1 });
 
 const InternshipApplication = mongoose.model(
