afbf583fd40e4bb1b3bf66133881d16dfe184d44
RyloRiz <91710711+RyloRiz@users.noreply.github.com>
2026-05-14 19:31:11 -0700

    fix: translate migration from ORM to SQL

--- migrations/20260509204247-add-platforms-to-event.js
+++ migrations/20260509204247-add-platforms-to-event.js
@@ -1,11 +1,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('events', 'platforms', {
-      type: Sequelize.ARRAY(Sequelize.STRING),
-      allowNull: false,
-      defaultValue: [],
-    });
+    // await queryInterface.addColumn('events', 'platforms', {
+    //   type: Sequelize.ARRAY(Sequelize.STRING),
+    //   allowNull: false,
+    //   defaultValue: [],
+    //   ifNotExists: true,
+    // });
+    await queryInterface.sequelize.query(`
+      ALTER TABLE "events"
+      ADD COLUMN IF NOT EXISTS "platforms" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[];
+    `);
   },
 
   async down(queryInterface, Sequelize) {
